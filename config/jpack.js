37b724e391f39ed672dea33731227f4221b8962c
Joffrey Demetz <joffrey.demetz@gmail.com>
2026-05-26 00:30:14 +0200

    theme via :root css vars + @container queries

--- config/jpack.js
+++ config/jpack.js
@@ -1,44 +1,15 @@
-import path from 'path';
-
-import {
-    LogMe,
-    jPackConfig,
-    generateLessVariablesFromConfig,
-    deleteLessVariablesFile
-} from 'jizy-packer';
+import { jPackConfig } from 'jizy-packer';
 
 const jPackData = function () {
-    const lessBuildVariablesPath = path.join(jPackConfig.get('basePath'), 'lib/less/_variables.less');
-
     jPackConfig.sets({
         name: 'jMessenger',
-        alias: 'jizy-messenger',
-        lessVariables: {
-            messengerFgColor: '#FFF',
-            messengerBackdropColor: 'rgba(0, 0, 0, .4)',
-            messengerSuccessBgColor: '#28a745',
-            messengerInfoBgColor: '#17a2b8',
-            messengerPrimaryBgColor: '#888',
-            messengerWarningBgColor: '#ff8400',
-            messengerDangerBgColor: '#dc3545'
-        }
+        alias: 'jizy-messenger'
     });
 
     jPackConfig.set('onCheckConfig', () => { });
-
-    jPackConfig.set('onGenerateBuildJs', (code) => {
-        LogMe.log('Build lib/less/_variables.less');
-        const lessVariables = jPackConfig.get('lessVariables') ?? {};
-        const lessOriginalVariablesPath = path.join(jPackConfig.get('basePath'), 'lib/less/variables.less');
-        generateLessVariablesFromConfig(lessOriginalVariablesPath, lessBuildVariablesPath, lessVariables);
-        return code;
-    });
-
+    jPackConfig.set('onGenerateBuildJs', (code) => code);
     jPackConfig.set('onGenerateWrappedJs', (wrapped) => wrapped);
-
-    jPackConfig.set('onPacked', () => {
-        deleteLessVariablesFile(lessBuildVariablesPath);
-    });
+    jPackConfig.set('onPacked', () => { });
 };
 
 export default jPackData;
